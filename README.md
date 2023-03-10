56de692aae8947027bacf335f5e08d873ad30c1b
juan pablo rojas chinchilla <jprojaschinchilla@gmail.com>
2023-03-09 20:53:16 -0600

    agregando readme y modificando archivo de creacion de base de datos

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # PruebaTecnica
 Prueba tecnica - Juan Pablo Rojas Chinchilla
+
+# Ejecucion
+Para le ejecucion del backend debe utilizarse el comando "npm run dev" o en su defecto compilar el codigo de typescript y correr el servidor con node como usualmente se hace. El archivo de ejecucion es Node/dist/index.js
+
+# Anotaciones
+En la prueba no pude terminar los delete, ya que tendria que hacerse una eliminacion en cascada dadas las relaciones utilizadas, estas relaciones se utilizaron de esta manera por motivos de no tener datos repetidos en base de datos
+
+# Consideraciones
+En caso no se termine el proyecto fue por limites de tiempo, ya que debia entregarse el programa realizado en un unico dia, dado que trabajo esta tarea solo fue realizada despues de mi horario laboral al igual que los test psicometricos.
--- SQL/creacion.sql
+++ SQL/creacion.sql
@@ -1,53 +1,53 @@
 -- Crear la tabla de Departamento
 CREATE TABLE Departamento (
-  idDepartamento INT NOT NULL AUTO_INCREMENT,
+  id INT NOT NULL AUTO_INCREMENT,
   nombreDepartamento VARCHAR(50) NOT NULL,
-  PRIMARY KEY (idDepartamento)
+  PRIMARY KEY (id)
 );
 
 -- Crear la tabla de Municipio
 CREATE TABLE Municipio (
-  idMunicipio INT NOT NULL AUTO_INCREMENT,
+  id INT NOT NULL AUTO_INCREMENT,
   nombreMunicipio VARCHAR(50) NOT NULL,
   idDepartamento INT NOT NULL,
-  PRIMARY KEY (idMunicipio),
-  FOREIGN KEY (idDepartamento) REFERENCES Departamento(idDepartamento)
+  PRIMARY KEY (id),
+  FOREIGN KEY (idDepartamento) REFERENCES Departamento(id)
 );
 
 -- Crear la tabla de Ubicacion
 CREATE TABLE Ubicacion (
-  idUbicacion INT NOT NULL AUTO_INCREMENT,
+  id INT NOT NULL AUTO_INCREMENT,
   direccion VARCHAR(100) NOT NULL,
   idMunicipio INT NOT NULL,
-  PRIMARY KEY (idUbicacion),
-  FOREIGN KEY (idMunicipio) REFERENCES Municipio(idMunicipio)
+  PRIMARY KEY (id),
+  FOREIGN KEY (idMunicipio) REFERENCES Municipio(id)
 );
 -- Crear la tabla de Categoria
 CREATE TABLE Categoria (
-  idCategoria INT NOT NULL AUTO_INCREMENT,
+  id INT NOT NULL AUTO_INCREMENT,
   nombreCategoria VARCHAR(50) NOT NULL,
   descripcionCategoria VARCHAR(100),
-  PRIMARY KEY (idCategoria)
+  PRIMARY KEY (id)
 );
 
 -- Crear la tabla de Producto
 CREATE TABLE Producto (
-  idProducto INT NOT NULL AUTO_INCREMENT,
+  id INT NOT NULL AUTO_INCREMENT,
   nombreProducto VARCHAR(50) NOT NULL,
   idCategoria INT NOT NULL,
-  PRIMARY KEY (idProducto),
-  FOREIGN KEY (idCategoria) REFERENCES Categoria(idCategoria)
+  PRIMARY KEY (id),
+  FOREIGN KEY (idCategoria) REFERENCES Categoria(id)
 );
 
 -- Crear la tabla de Sucursal
 CREATE TABLE Sucursal (
-  idSucursal INT NOT NULL AUTO_INCREMENT,
+  id INT NOT NULL AUTO_INCREMENT,
   nombreSucursal VARCHAR(50) NOT NULL,
   correoSucursal VARCHAR(50),
   telefonoSucursal VARCHAR(20),
   idUbicacion INT NOT NULL,
-  PRIMARY KEY (idSucursal),
-  FOREIGN KEY (idUbicacion) REFERENCES Ubicacion(idUbicacion)
+  PRIMARY KEY (id),
+  FOREIGN KEY (idUbicacion) REFERENCES Ubicacion(id)
 );
 
 
